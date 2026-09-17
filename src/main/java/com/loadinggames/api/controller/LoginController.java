e408dd3fb5c5b1c1fbc59f17f7b16b4b8917a779
Yasmim Santos <yasmimgrilo@hotmail.com>
2026-09-17 20:40:54 -0300

    Atualizando

---
+++ src/main/java/com/loadinggames/api/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.loadinggames.api.controller;
+
+import com.loadinggames.api.model.Login;
+import com.loadinggames.api.repository.LoginRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    LoginRepository repository;
+    @PostMapping("cadastro")
+    public Login cadastro(@RequestBody Login login) {
+        return repository.save(login);
+    }
+}
---
+++ src/main/java/com/loadinggames/api/repository/LoginRepository.java
@@ -0,0 +1,9 @@
+package com.loadinggames.api.repository;
+
+import com.loadinggames.api.model.Login;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoginRepository extends JpaRepository <Login, Long>{
+}
